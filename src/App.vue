7a7cda22818a1fb4784bea936d389964c3eecbda
Artem Pronin <artemalexp@gmail.com>
2024-04-30 15:15:15 +0300

    Added header component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue'
+</script>
 
 <template>
-  <h1>Hello, Vue</h1>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+    <h1>Hello, Vue</h1>
+  </div>
 </template>
