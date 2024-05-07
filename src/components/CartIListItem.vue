6a6f161f6ae21f63197224237a227db1429f0fe4
Artem Pronin <artemalexp@gmail.com>
2024-05-07 20:47:15 +0300

    Drawer created

---
+++ src/components/CartIListItem.vue
@@ -0,0 +1,14 @@
+<script setup>
+import CartItem from './CartItem.vue'
+</script>
+
+<template>
+  <ul class="flex flex-col gap-5">
+    <CartItem />
+    <CartItem />
+    <CartItem />
+    <CartItem />
+    <CartItem />
+    <CartItem />
+  </ul>
+</template>
