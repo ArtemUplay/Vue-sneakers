8c6eb463848d5a864957145be0237f67d97d6bd6
Artem Pronin <artemalexp@gmail.com>
2024-05-09 14:15:50 +0300

    Created search and select

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,25 @@ import Drawer from './components/Drawer.vue'
     <Header />
 
     <main class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+            <option>По названию</option>
+            <option>По цене (сначала дешёвые)</option>
+            <option>По цене (сначала дорогие)</option>
+          </select>
+          <div class="relative">
+            <img class="absolute left-4 top-3.5 cursor-pointer" src="/search.svg" alt="Search" />
+            <input
+              class="border border-gray-200 rounded-md py-2 pl-12 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
+      </div>
       <CardList />
     </main>
   </div>
